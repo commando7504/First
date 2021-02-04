17de73ec13840f4bfe3845dffead44fc3a169427
commando7504 <67062093+commando7504@users.noreply.github.com>
2021-02-04 11:14:06 +0530

    Initial commit

---
+++ src/qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"601b88ecbfd603001f71b988",
+				"601b88ecbfd603001f71b98a"
+			]
+		},
+		"601b88ecbfd603001f71b988": {
+			"id": "601b88ecbfd603001f71b988",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"601b88ecbfd603001f71b98a": {
+			"id": "601b88ecbfd603001f71b98a",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
